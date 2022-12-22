393052fd91dca8cf96297cffafcba154c299e0db
Pavel Shalimov <pabushabi1@gmail.com>
2022-12-22 23:46:47 +0300

    Added share & new background

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hyperion/parallax/parallax_screenshot.dart';
+import 'package:share_plus/share_plus.dart';
 
 class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
@@ -32,106 +33,115 @@ class _AppState extends State<App> {
       ),
       body: Center(
         child: SingleChildScrollView(
-          padding: EdgeInsets.symmetric(
-              horizontal: MediaQuery.of(context).size.height * .3),
+          // padding: EdgeInsets.symmetric(
+          //     horizontal: MediaQuery.of(context).size.height * .3),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
+            children: [
               ClipRect(
                 child: Align(
                   alignment: Alignment.center,
-                  heightFactor: 0.5,
-                  child: Image.network('https://raw.githubusercontent.com/pabushabi/hyperion_web/main/assets/images/background.jpg'),
+                  heightFactor: 0.33,
+                  child: Image.network(
+                      'https://raw.githubusercontent.com/pabushabi/hyperion_web/main/assets/images/background.jpg'),
                 ),
               ),
-              // 1-й заголовок
-              Text(
-                'Змейка',
-                style: Theme.of(context).textTheme.headline3?.copyWith(
-                      fontFamily: 'Unbounded',
-                      color: Theme.of(context).colorScheme.onBackground,
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.height * .3),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    // 1-й заголовок
+                    Text(
+                      'Змейка',
+                      style: Theme.of(context).textTheme.headline3?.copyWith(
+                            fontFamily: 'Unbounded',
+                            color: Theme.of(context).colorScheme.onBackground,
+                          ),
                     ),
-              ),
-              const SizedBox(height: 12),
-              // 1-й абзац
-              const Text(
-                'ПАшка молодец, мы пытались...., '
-                'sed do eiusmod tempor incididunt ut labore et dolore magna '
-                'aliqua. Ut enim ad minim veniam, quis nostrud exercitation '
-                'ullamco laboris nisi ut aliquip ex ea commodo consequat. '
-                'Duis aute irure dolor in reprehenderit in voluptate velit '
-                'esse cillum dolore eu fugiat nulla pariatur. Excepteur sint '
-                'occaecat cupidatat non proident, sunt in culpa qui officia '
-                'deserunt mollit anim id est laborum.',
-                style: TextStyle(fontSize: 20),
-              ),
-              const SizedBox(height: 24),
-              // 2-й заголовок
-              Text(
-                'HEADLINE 2',
-                style: Theme.of(context).textTheme.headline4?.copyWith(
-                      fontFamily: 'Unbounded',
-                      color: Theme.of(context).colorScheme.onBackground,
+                    const SizedBox(height: 12),
+                    // 1-й абзац
+                    const Text(
+                      'ПАшка молодец, мы пытались...., '
+                      'sed do eiusmod tempor incididunt ut labore et dolore magna '
+                      'aliqua. Ut enim ad minim veniam, quis nostrud exercitation '
+                      'ullamco laboris nisi ut aliquip ex ea commodo consequat. '
+                      'Duis aute irure dolor in reprehenderit in voluptate velit '
+                      'esse cillum dolore eu fugiat nulla pariatur. Excepteur sint '
+                      'occaecat cupidatat non proident, sunt in culpa qui officia '
+                      'deserunt mollit anim id est laborum.',
+                      style: TextStyle(fontSize: 20),
                     ),
-              ),
-              const SizedBox(height: 16),
-              // 2-й абзац
-              const Text(
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, '
-                'sed do eiusmod tempor incididunt ut labore et dolore magna '
-                'aliqua. Ut enim ad minim veniam, quis nostrud exercitation '
-                'ullamco laboris nisi ut aliquip ex ea commodo consequat. '
-                'Duis aute irure dolor in reprehenderit in voluptate velit '
-                'esse cillum dolore eu fugiat nulla pariatur. Excepteur sint '
-                'occaecat cupidatat non proident, sunt in culpa qui officia '
-                'deserunt mollit anim id est laborum.',
-                style: TextStyle(fontSize: 20),
-              ),
-              const SizedBox(height: 24),
-              ParallaxScreenshot(
-                image:
-                    'https://raw.githubusercontent.com/pabushabi/hyperion_web/main/assets/images/screenshot1.jpg',
-                title: 'Скриншот 1',
-                subtitle: 'description',
-                isNetwork: true,
-              ),
-              ParallaxScreenshot(
-                image:
-                    'https://raw.githubusercontent.com/pabushabi/hyperion_web/main/assets/images/screenshot2.jpg',
-                title: 'Скриншот 2',
-                subtitle: 'description',
-                isNetwork: true,
-              ),
-              ParallaxScreenshot(
-                image:
-                    'https://raw.githubusercontent.com/pabushabi/hyperion_web/main/assets/images/screenshot3.jpg',
-                title: 'Скриншот 3',
-                subtitle: 'description',
-                isNetwork: true,
-              ),
-              const SizedBox(height: 24),
-              // 3-й заголовок
-              Text(
-                'HEADLINE',
-                style: Theme.of(context).textTheme.headline4?.copyWith(
-                      fontFamily: 'Unbounded',
-                      color: Theme.of(context).colorScheme.onBackground,
+                    const SizedBox(height: 24),
+                    // 2-й заголовок
+                    Text(
+                      'HEADLINE 2',
+                      style: Theme.of(context).textTheme.headline4?.copyWith(
+                            fontFamily: 'Unbounded',
+                            color: Theme.of(context).colorScheme.onBackground,
+                          ),
                     ),
+                    const SizedBox(height: 16),
+                    // 2-й абзац
+                    const Text(
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, '
+                      'sed do eiusmod tempor incididunt ut labore et dolore magna '
+                      'aliqua. Ut enim ad minim veniam, quis nostrud exercitation '
+                      'ullamco laboris nisi ut aliquip ex ea commodo consequat. '
+                      'Duis aute irure dolor in reprehenderit in voluptate velit '
+                      'esse cillum dolore eu fugiat nulla pariatur. Excepteur sint '
+                      'occaecat cupidatat non proident, sunt in culpa qui officia '
+                      'deserunt mollit anim id est laborum.',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    const SizedBox(height: 24),
+                    ParallaxScreenshot(
+                      image:
+                          'https://raw.githubusercontent.com/pabushabi/hyperion_web/main/assets/images/screenshot1.jpg',
+                      title: 'Скриншот 1',
+                      subtitle: 'description',
+                      isNetwork: true,
+                    ),
+                    ParallaxScreenshot(
+                      image:
+                          'https://raw.githubusercontent.com/pabushabi/hyperion_web/main/assets/images/screenshot2.jpg',
+                      title: 'Скриншот 2',
+                      subtitle: 'description',
+                      isNetwork: true,
+                    ),
+                    ParallaxScreenshot(
+                      image:
+                          'https://raw.githubusercontent.com/pabushabi/hyperion_web/main/assets/images/screenshot3.jpg',
+                      title: 'Скриншот 3',
+                      subtitle: 'description',
+                      isNetwork: true,
+                    ),
+                    const SizedBox(height: 24),
+                    // 3-й заголовок
+                    Text(
+                      'HEADLINE',
+                      style: Theme.of(context).textTheme.headline4?.copyWith(
+                            fontFamily: 'Unbounded',
+                            color: Theme.of(context).colorScheme.onBackground,
+                          ),
+                    ),
+                    const SizedBox(height: 16),
+                    // Абзац
+                    const Text(
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, '
+                      'sed do eiusmod tempor incididunt ut labore et dolore magna '
+                      'aliqua. Ut enim ad minim veniam, quis nostrud exercitation '
+                      'ullamco laboris nisi ut aliquip ex ea commodo consequat. '
+                      'Duis aute irure dolor in reprehenderit in voluptate velit '
+                      'esse cillum dolore eu fugiat nulla pariatur. Excepteur sint '
+                      'occaecat cupidatat non proident, sunt in culpa qui officia '
+                      'deserunt mollit anim id est laborum.',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    const SizedBox(height: 124),
+                  ],
+                ),
               ),
-              const SizedBox(height: 16),
-              // Абзац
-              const Text(
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, '
-                'sed do eiusmod tempor incididunt ut labore et dolore magna '
-                'aliqua. Ut enim ad minim veniam, quis nostrud exercitation '
-                'ullamco laboris nisi ut aliquip ex ea commodo consequat. '
-                'Duis aute irure dolor in reprehenderit in voluptate velit '
-                'esse cillum dolore eu fugiat nulla pariatur. Excepteur sint '
-                'occaecat cupidatat non proident, sunt in culpa qui officia '
-                'deserunt mollit anim id est laborum.',
-                style: TextStyle(fontSize: 20),
-              ),
-              const SizedBox(height: 124),
             ],
           ),
         ),
@@ -142,7 +152,10 @@ class _AppState extends State<App> {
           mainAxisSize: MainAxisSize.min,
           children: [
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                Share.share(
+                    'Посмотрите какой крутой проект! https://hyperion.codemagic.app');
+              },
               icon: Icon(Icons.share),
             ),
             // IconButton(
